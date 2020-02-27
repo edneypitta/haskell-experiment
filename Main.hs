77b0e3ee2c800c0d8a8328cc4aa75ddd8ec66ff3
Edney Pitta <edney.pitta@farfetch.com>
2020-02-27 20:50:12 +0000

    read input commands

--- Main.hs
+++ Main.hs
@@ -1,42 +1,41 @@
 module Main where
 
-  import Data.HashSet
-  import Text.Read
-  import Data.Maybe
-  import System.Console.Haskeline
-  import Control.Monad
-  
-  data Direction = North | East | South | West deriving (Eq, Show, Read)
+import Data.HashSet
+import Text.Read
+import Data.Maybe
+import System.Console.Readline
+import Control.Monad
+import Control.Monad.IO.Class
 
-  data Command = Command 
-    { direction :: Direction
-    , steps :: Integer 
-    } deriving (Eq, Show, Read)
+data Direction = North | East | South | West deriving (Eq, Show, Read)
 
-  data Robot = Robot 
-    { position :: (Integer, Integer)
-    , commands :: [Command] 
-    } deriving (Eq, Show, Read)
+data Command = Command 
+  { direction :: Direction
+  , steps     :: Integer 
+  } deriving (Eq, Show, Read)
 
-  main :: IO ()
-  main = runInputT defaultSettings loop
-    where
-      loop :: InputT IO ()
-      loop = do
-        maybeNumberOfCommands <- getInputLine ""
-        position <- getInputLine ""
+data Robot = Robot 
+  { position :: (Integer, Integer)
+  , commands :: [Command] 
+  } deriving (Eq, Show, Read)
 
-        let numberOfCommands = fromMaybe 0 $ maybeNumberOfCommands >>= readMaybe
-        --let h = Prelude.map Nothing [0..numberOfCommands] :: Maybe String
-        let q = foldM(\acc _ -> do
-                                  input <- getInputLine ""
-                                  outputStrLn $ input
-                                  return acc:input) [] replicate numberOfCommands Nothing
+getInputs :: Integer -> [Maybe String] -> IO [Maybe String]
+getInputs 0 acc = return acc
+getInputs n acc = do
+                    input <- readline ""
+                    getInputs (n - 1) (acc ++ [input])
 
-        
-        case position of
-          Nothing -> return ()
-          Just "quit" -> return ()
-          Just input -> do 
-                          outputStrLn $ "Input was: " ++ input
-                          loop
+main :: IO ()
+main = do
+          maybeNumberOfCommands <- readline ""
+          position <- readline ""
+
+          let numberOfCommands = fromMaybe 0 $ maybeNumberOfCommands >>= readMaybe
+          commands <- getInputs numberOfCommands []
+          
+          case position of
+            Nothing -> return ()
+            Just "quit" -> return ()
+            Just input -> do 
+                            putStrLn $ "Input was: " ++ input
+                            main
