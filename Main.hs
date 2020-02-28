6fd08124eb51fbb1e3b96c1936b01c1c2d618e8c
Edney Pitta <edney.pitta@farfetch.com>
2020-02-28 14:42:58 +0000

    parse commands

--- Main.hs
+++ Main.hs
@@ -30,10 +30,31 @@ ms2i :: Maybe String -> Integer
 ms2i Nothing  = 0
 ms2i (Just s) = read s
 
-getPosition :: Maybe String -> Maybe (Integer, Integer)
-getPosition Nothing  = Nothing
-getPosition (Just s) = Just (read fst, read snd)
-                       where fst:snd:[] = splitOn " " s
+parsePosition :: Maybe String -> Maybe (Integer, Integer)
+parsePosition Nothing  = Nothing
+parsePosition (Just s) = Just (read fst, read snd)
+                         where fst:snd:[] = splitOn " " s
+
+parseDirection :: String -> Maybe Direction
+parseDirection ('N':[]) = Just North
+parseDirection ('E':[]) = Just East
+parseDirection ('S':[]) = Just South
+parseDirection ('W':[]) = Just West
+parseDirection _        = Nothing
+
+parseCommand :: String -> Maybe Command
+parseCommand s = case direction of
+                   Nothing -> Nothing
+                   Just d  -> Just Command {direction = d, steps = steps}
+                 where fst:snd:[] = splitOn " " s
+                       direction  = parseDirection fst
+                       steps      = read snd 
+
+parseCommands :: [Maybe String] -> [Maybe Command] -> Maybe [Command]
+parseCommands [] acc     = sequence acc
+parseCommands (c:cs) acc = case c of
+                            Nothing -> parseCommands cs acc
+                            Just s  -> parseCommands cs (acc ++ [parseCommand s])
 
 main :: IO ()
 main = do
